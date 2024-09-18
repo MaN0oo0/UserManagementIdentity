38c5f7305798bf9028d7741b72a550a6118b65a4
MaN0oo0 <orbanm316@gmail.com>
2024-09-18 14:38:19 +0300

    Upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UserManagmentWithIdentity.Models;
 
 namespace UserManagmentWithIdentity.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //to change Name of table after migration
+            builder.Entity<ApplicationUser>().ToTable("Users", "security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "security");
         }
     }
 }
--- Data/Migrations/20240917165952_AddNewColumnsAtUsersTable.Designer.cs
+++ Data/Migrations/20240917165952_AddNewColumnsAtUsersTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserManagmentWithIdentity.Data;
 
@@ -11,9 +12,11 @@ using UserManagmentWithIdentity.Data;
 namespace UserManagmentWithIdentity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240917165952_AddNewColumnsAtUsersTable")]
+    partial class AddNewColumnsAtUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -208,6 +211,7 @@ namespace UserManagmentWithIdentity.Data.Migrations
                         .HasColumnType("bit");
 
                     b.Property<byte[]>("ProfilePicture")
+                        .IsRequired()
                         .HasColumnType("varbinary(max)");
 
                     b.Property<string>("SecurityStamp")
